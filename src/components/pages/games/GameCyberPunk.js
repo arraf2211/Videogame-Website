a21cb78e8166728f0364f3d4cc1b1dc7888b66f6
Rahman, Arraf (k21034926) <arrafrahman22@gmail.com>
2023-08-05 20:37:28 +0100

    added reviews for god and cyber

--- src/components/pages/games/GameCyberPunk.js
+++ src/components/pages/games/GameCyberPunk.js
@@ -25,11 +25,20 @@ function GameCyberPunk() {
           <div className='cards Cyber'>
             <ul className='review__CardLists'>
                 <ReviewSection
-                header='Review'
-                text='Coming Soon ...'/>
+                header='Review - Story'
+                text='Cyberpunk 2077 is about mercenary, V, who after a terrible heist gets implanted with a chip which threatens to overwrite his entire personality and memories of Johnny Sliverhand, and is about the two of them trying to get separated and to save V. The characters you meet along the way are great, such as Johnny Sliverhand who, to put it simply, is a rockstar who hates corporations for what they did to the world and to him, or the reckless but caring nomad Panam, who have so much depth to them making them feel so human and real. The story is action packed with a great pacing and wide range of player choices encouraging multiple playthroughs to try out different paths. The story really stays with you with each choice being meaningful especially the ending which had me conflicted with what to do and how it would affect V’s loved ones, CyberPunk’s story is another great story created by CD Project Red.'/>
+                <ReviewSection
+                header='Soundtrack'
+                text='The soundtrack for cyberpunk is techno, heavy hitting and absolutely amazing to listen to, such as the track used for Sliverhand’s missions which makes you feel like an absolute badass rockstar. The soundtrack is just great and is a must listen.'/>
+                <ReviewSection
+                header='Gameplay'
+                text="With this being a RPG there is a wide arrange of approaches one can take such as being an invisible katana wielding double jumping assassin, or a cocky netrunner who can cause enemies to kill themselves with one glance to a shotgun wielding psycho who can with the assist of his cyber implement slow down time to get right behind of an unexpecting gangster, the possibilities are limitless. Having so much choice of what character build you want, will make you want to play the game again and again. With my 40 hours of game time, I never once felt bored due to the side quests, weapons to find and due to the dream cars which you could obtain and drive around such as the beautiful Porsche 911 Turbo. The only thing which I disliked was the vehicle control, I hated how the driving was due to how bad it felt, they felt more like toy cars than cars of the future yet this still didn't stop me from exploring every nook and cranny to find my next car. The city where this all takes place, Night City is a mesmerising place to drive through showing this ultra capitalistic city with large neon adverts across every skyscraper, but down to the streets is teeming with gangs, crime and corpo’s making it enjoyable to explore and find hidden secrets or powerful weapons. "/>
+                <ReviewSection
+                header='Graphics'
+                text='The game is just beautiful and this is due to how well made Night City is, it really is a pro-Capitalist dystopia and it makes you want to explore it and to get lost in it. '/>
                 <ReviewSection
                 header='Score'
-                text='8/10'/>
+                text='9/10'/>
                 </ul>
             <div className='cards__container'>
                 <div className='cards__wrapper'>
--- src/components/pages/games/GameGOW.js
+++ src/components/pages/games/GameGOW.js
@@ -22,11 +22,24 @@ function GameGOW() {
           <div className='cards GOW'>
             <ul className='review__CardLists'>
                     <ReviewSection
-                    header='Review'
-                    text='Coming Soon ...'/>
+                    header='Review - Story'
+                    text=" God of War Ragnarök's story is really compelling and follows a few years after the events of the last game and shows the consequences and how Kratos and Aretus battle with their own fates to try and save the Realms. I loved the twists and turns and the renditions of famous characters that it introduced such as Thor and Odin. This pair aren't like their Marvel counterparts, Thor is shown as this massive drunk war machine and Odin is
+                    … ahh I’ll let you find out for yourself. The main man himself Kratos is once again an amazing protagonist to play as, and players can enjoy witnessing his continued battle to overcome his violent past. The story however has some pacing issues being a 25 hour long game; there were some parts of the game which were jarring to get through (anyone who has played the game knows exactly which part I'm talking about…) however, after that, the pacing  picks up again and it concludes amazingly.
+                                         "/>
+                    <ReviewSection
+                    header='Soundtrack'
+                    text="I'm not much of a music person but my god Bear McCreary has created another masterpiece; the soundtrack is amazing once again, no skips on any of the songs.
+                    "/>
+                    <ReviewSection
+                    header='Gameplay'
+                    text="God of War (2018) was a good game with decent combat but after awhile it had gotten boring and repetitive with the same enemies and boss fights (god I hated fighting those trolls as boss fights) however with Ragnarok I am glad to say that the combat was vastly improved upon with much more faster paced combat and with a great number of enemies to unleash Kratos’ rage upon. The boss fights here may get a bit tiring especially post end, as the boss fights are gonna be of different variants of bosses you’ve already faced but it’s still great and rewarding to do. There’s still a few puzzles to do such as the Normir chests and environmental puzzles, whilst these are fun to do especially the Normir chests, the environmental puzzles can be annoying to do mainly due to Aretus hinting to you on what you have to do every 5 minutes, I really wish there was an option to get rid of hints for those puzzles. With the realms you get to travel to there is a great amount to explore and quests to do in them such as Vanaheim and Midgard which is just packed to the brim with quests and secrets to find. But this isn't the same with all Realms, since some are only for the main story and some are very small in size with only a few quests to do in them. Exploring is fun to do and I loved finding new side quests or new pieces of armour to equip on Kratos but the only issue I had with exploring was the compass on the HUD which sometimes made me extremely lost and often felt like it was fighting with me but this had only happened on a small amount of occasions. 
+                    "/>
+                    <ReviewSection
+                    header='Graphics'
+                    text="Beautiful, just beautiful and stunning. The realms that you travel to in this game are vast with each one different to one another, from Jungles of Vanaheim to the snow covered Midgard, each realm is beautiful, trust me when I say this that photo mode will be used a lot in some of these locations. "/>
                     <ReviewSection
                     header='Score'
-                    text='9/10'/>
+                    text='8.5/10'/>
             </ul>
             <div className='cards__container'>
                 <div className='cards__wrapper'>
